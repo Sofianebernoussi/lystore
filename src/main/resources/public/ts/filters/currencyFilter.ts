dff1103b147b7319c6df2a5542b19f2a178f1841
agnes.lapeyronnie <agnes.lapeyronnie@cgi.com>
2018-02-27 14:50:36 +0100

    Fix : image, name and quanty of equipment's option in list of order

--- src/main/resources/public/ts/filters/currencyFilter.ts
+++ src/main/resources/public/ts/filters/currencyFilter.ts
@@ -1,7 +1,11 @@
 import { ng, idiom } from 'entcore';
 
 export const currency = ng.filter('currency', () =>
-    (amount) => (parseFloat(amount).toLocaleString(undefined,
-        {minimumFractionDigits: 2, maximumFractionDigits: 2}) +
-        ' ' + idiom.translate('money.symbol'))
+    (amount) => {
+        return (isNaN((parseFloat(amount))) ?
+            0 :
+            parseFloat(amount).toLocaleString(undefined,
+                {minimumFractionDigits: 2, maximumFractionDigits: 2})) +
+            ' ' + idiom.translate('money.symbol');
+    }
 );

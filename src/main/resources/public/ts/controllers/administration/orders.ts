14d08f9b0f2f9b331c2e40894f7fb4924fa565e6
LEDUNOIS Simon <simon.ledunois@cgi.com>
2018-04-04 09:28:14 +0200

    Fix types & Fix agent display list

--- src/main/resources/public/ts/controllers/administration/orders.ts
+++ src/main/resources/public/ts/controllers/administration/orders.ts
@@ -77,7 +77,7 @@ export const orderController = ng.controller('orderController',
             ordersToWindUp.all = Mix.castArrayAs(OrderClient, orders);
             let { status } = await ordersToWindUp.updateStatus('DONE');
             if (status === 200) {
-                $scope.notifications.push(new Notification( 'lystore.windUp.notif' , 'confirm'));
+                $scope.notifications.push(new Notification('lystore.windUp.notif', 'confirm'));
             }
             await $scope.ordersClient.sync($scope.structures.all);
             $scope.ordersClient.all = _.where($scope.ordersClient.all, {status: 'SENT'});

040ee5765af54a3dba422571f7f2f268a0ca6cb8
mamoudou.diallo <Cgi192018>
2018-12-14 14:14:37 +0100

    MLYSSTORE-174 : update of the preference = id field whose value is null (011-Lystore.sql)

--- src/main/resources/sql/011-lystore.sql
+++ src/main/resources/sql/011-lystore.sql
@@ -23,3 +23,7 @@ ALTER TABLE lystore.project
 
 ALTER TABLE lystore.campaign
   ADD COLUMN priority_enabled boolean DEFAULT true ;
+
+UPDATE lystore.project
+  SET preference=id
+WHERE preference IS NULL;

1e2ac6a35d1d9cfbc3ed1a79a6fd1d6fe84bfce3
RhitaRah <rahni.rhita@gmail.com>
2019-07-10 11:47:47 +0200

    Mlystore-272 create data base of order-region-equipment

---
+++ src/main/resources/sql/024-lystore-order-client-equipment.sql
@@ -0,0 +1,52 @@
+CREATE TABLE lystore."order-region-equipment"
+(
+    id bigserial NOT NULL,
+    price numeric NOT NULL,
+    amount bigint NOT NULL,
+    creation_date date NOT NULL,
+    modification_date date,
+    owner_name character varying NOT NULL,
+    owner_id character varying NOT NULL,
+    name character varying(255) NOT NULL,
+    "summary " character varying(300) NOT NULL,
+    description text NOT NULL,
+    image character varying(100) NOT NULL,
+    technical_spec json NOT NULL,
+    status character varying(50),
+    id_contract bigint,
+    equipment_key bigint NOT NULL,
+    id_campaigne bigint,
+    id_structure character,
+    cause_status character varying(300),
+    number_validation character varying(50),
+    id_order bigint,
+    comment text,
+    rank numeric,
+    id_project bigint,
+    id_order_client_equipment bigint,
+    CONSTRAINT order_region_equipment_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_campaign_id FOREIGN KEY (id_campaigne)
+        REFERENCES lystore.campaign (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_contract_id FOREIGN KEY (id_contract)
+        REFERENCES lystore.contract (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_order_id FOREIGN KEY (id_order)
+        REFERENCES lystore."order" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_project_id FOREIGN KEY (id_project)
+        REFERENCES lystore.project (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_order_client_id FOREIGN KEY (id_order_client_equipment)
+        REFERENCES lystore.order_client_equipment (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT status_values CHECK ((status::text = ANY (ARRAY['WAITING'::character varying::text, 'VALID'::character varying::text, 'WAITING_FOR_ACCEPTANCE'::character varying::text, 'REJECTED'::character varying::text, 'SENT'::character varying::text, 'DONE'::character varying::text]))) NOT VALID,
+    CONSTRAINT "Check_price_positive" CHECK (price >= 0::numeric) NOT VALID,
+    CONSTRAINT "Check_amount_positive" CHECK (amount::numeric >= 0::numeric) NOT VALID,
+    CONSTRAINT "Check_order_or_campaigne_structure" CHECK (id_order_client_equipment is not null or ( id_structure is not null  and id_campaigne is not null) ) NOT VALID
+)

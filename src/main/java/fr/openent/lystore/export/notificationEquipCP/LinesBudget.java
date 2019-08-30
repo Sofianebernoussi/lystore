09bafc6bf0aea77494833be8dcda7be1a06c9306
Rseuret <remy@seuret.com>
2019-08-30 09:14:07 +0200

    adding final handlers when error + unique id sent to noe4j

--- src/main/java/fr/openent/lystore/export/notificationEquipCP/LinesBudget.java
+++ src/main/java/fr/openent/lystore/export/notificationEquipCP/LinesBudget.java
@@ -39,7 +39,8 @@ public class LinesBudget extends TabHelper {
             JsonArray actions = new JsonArray(data.getString("actions"));
             for (int j = 0; j < actions.size(); j++) {
                 JsonObject action = actions.getJsonObject(j);
-                structuresId.add(structuresId.size(), action.getString("id_structure"));
+                if(!structuresId.contains(action.getString("id_structure")))
+                    structuresId.add(structuresId.size(), action.getString("id_structure"));
 
             }
         }

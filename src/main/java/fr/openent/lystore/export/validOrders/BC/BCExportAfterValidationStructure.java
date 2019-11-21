dfd10d0e230b08715f86092ea91fb14569ee2392
Rseuret <remy@seuret.com>
2019-11-21 11:17:52 +0100

    finished template and datas send

--- src/main/java/fr/openent/lystore/export/validOrders/BC/BCExportAfterValidationStructure.java
+++ src/main/java/fr/openent/lystore/export/validOrders/BC/BCExportAfterValidationStructure.java
@@ -1,6 +1,7 @@
 package fr.openent.lystore.export.validOrders.BC;
 
 import fr.openent.lystore.Lystore;
+import fr.openent.lystore.controllers.OrderController;
 import fr.openent.lystore.export.validOrders.PDF_OrderHElper;
 import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
@@ -15,6 +16,11 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import org.entcore.common.sql.Sql;
 import org.entcore.common.sql.SqlResult;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class BCExportAfterValidationStructure extends PDF_OrderHElper {
     private Logger log = LoggerFactory.getLogger(BCExport.class);
@@ -64,6 +70,121 @@ public class BCExportAfterValidationStructure extends PDF_OrderHElper {
 
         );
 
+    }
+    @Override
+    protected void retrieveOrderData(final Handler<Either<String, Buffer>> exportHandler, JsonArray ids,boolean groupByStructure,
+                                       final Handler<JsonObject> handler) {
+        orderService.getOrders(ids, null, true, groupByStructure, new Handler<Either<String, JsonArray>>() {
+            @Override
+            public void handle(Either<String, JsonArray> event) {
+                if (event.isRight()) {
+                    JsonObject order = new JsonObject();
+                    ArrayList <String> listStruct = new ArrayList<>();
+                    JsonArray ordersStructured = new JsonArray();
+                    JsonArray orders = OrderController.formatOrders(event.right().getValue());
+                    orders = sortByUai(orders);
+                    for(int i=0;i<orders.size();i++){
+                    JsonObject orderSorted = orders.getJsonObject(i);
+                    String idStruct = orderSorted.getString("id_structure");
+                        if(order.containsKey(idStruct)){
+                            JsonArray tempOrders = order.getJsonObject(idStruct).getJsonArray("orders").add(orderSorted);
+                            order.put(orderSorted.getString("id_structure"),new JsonObject().put("orders",tempOrders));
+                        }else{
+                            listStruct.add(idStruct);
+                            order.put(orderSorted.getString("id_structure"),new JsonObject().put("orders", new JsonArray().add(orderSorted)));
+                        }
+                    }
+                    for (String s : listStruct) {
+                        JsonObject ordersByStructure = order.getJsonObject(s);
+                        Double sumWithoutTaxes = getSumWithoutTaxes(ordersByStructure.getJsonArray("orders"));
+                        Double taxTotal = getTaxesTotal(ordersByStructure.getJsonArray("orders"));
+
+                        ordersByStructure.put("sumLocale",
+                                OrderController.getReadableNumber(OrderController.roundWith2Decimals(sumWithoutTaxes)));
+                        ordersByStructure.put("totalTaxesLocale",
+                                OrderController.getReadableNumber(OrderController.roundWith2Decimals(taxTotal)));
+                        ordersByStructure.put("totalPriceTaxeIncludedLocal",
+                                OrderController.getReadableNumber(OrderController.roundWith2Decimals(taxTotal + sumWithoutTaxes)));
+                        order.put(s, ordersByStructure);
+                    }
+
+                    structureService.getStructureById(new JsonArray(listStruct), new Handler<Either<String, JsonArray>>() {
+                        @Override
+                        public void handle(Either<String, JsonArray> event) {
+                            if (event.isRight()) {
+                                JsonArray structures = event.right().getValue();
+                                JsonObject structure ;
+                                for (int i = 0; i < structures.size(); i++) {
+                                    structure = structures.getJsonObject(i);
+                                    JsonObject ordersByStructure = order.getJsonObject(structure.getString("id"));
+                                    ordersByStructure.put("name",structure.getString("name"));
+                                    ordersByStructure.put("uai",structure.getString("uai"));
+                                    ordersByStructure.put("address",structure.getString("address"));
+                                    ordersByStructure.put("phone",structure.getString("phone"));
+                                    order.put(structure.getString("id"),ordersByStructure);
+
+                                }
+
+                                JsonArray ordersArray = new JsonArray();
+                                for (String idStruct : listStruct) {
+                                    JsonObject ordersJsonObject = order.getJsonObject(idStruct);
+                                    ordersArray.add(ordersJsonObject);
+                                    order.remove(idStruct);
+                                }
+                                order.put("orderArray",ordersArray);
+                                handler.handle(order);
+                            } else {
+                                log.error("An error occurred when collecting structures based on ids");
+                                exportHandler.handle(new Either.Left<>("An error occurred when collecting structures based on ids"));
+
+                            }
+                        }
+                    });
+
+
+
+                } else {
+                    log.error("An error occurred when retrieving order data");
+                    exportHandler.handle(new Either.Left<>("An error occurred when retrieving order data"));
+                }
+            }
+        });
+    }
+
+    private JsonArray sortByUai(JsonArray values) {
+        JsonArray sortedJsonArray = new JsonArray();
+
+        List<JsonObject> jsonValues = new ArrayList<JsonObject>();
+        for (int i = 0; i < values.size(); i++) {
+            jsonValues.add(values.getJsonObject(i));
+        }
+
+        Collections.sort(jsonValues, new Comparator<JsonObject>() {
+            private static final String KEY_NAME = "id_structure";
+
+            @Override
+            public int compare(JsonObject a, JsonObject b) {
+                String valA = "";
+                String valB = "";
+                try {
+                    if (a.containsKey(KEY_NAME)) {
+                        valA = a.getString(KEY_NAME);
+                    }
+                    if (b.containsKey(KEY_NAME)) {
+                        valB = b.getString(KEY_NAME);
+                    }
+                } catch (NullPointerException e) {
+                    log.error("error when sorting structures during export");
+                }
+
+                return valA.compareTo(valB);
+            }
+        });
+
+        for (int i = 0; i < values.size(); i++) {
+            sortedJsonArray.add(jsonValues.get(i));
+        }
+        return sortedJsonArray;
     }
 
     private void getOrdersDataSql(String nbrbc, Handler<Either<String,JsonArray>> handler) {

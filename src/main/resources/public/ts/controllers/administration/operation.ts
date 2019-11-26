eb0c01aee3ecef4cafd4c59ae4ad3dfb0b966ddc
Rseuret <remy@seuret.com>
2019-11-26 11:02:32 +0100

    MLYSTOR-348 -- fixing  default date

--- src/main/resources/public/ts/controllers/administration/operation.ts
+++ src/main/resources/public/ts/controllers/administration/operation.ts
@@ -76,7 +76,9 @@ export const operationController = ng.controller('operationController',
             let operationMomentDate = moment(operation.date_cp);
             let isValid = true;
             $scope.operations.all.forEach(operationn => {
-                if(moment(operationMomentDate).format("DD/MM/YYYY") === moment(operationn.date_cp).format("DD/MM/YYYY") && operationn.id != operation.id){
+                if(!isNaN(operationMomentDate)
+                    && moment(operationMomentDate).format("DD/MM/YYYY") === moment(operationn.date_cp).format("DD/MM/YYYY")
+                    && operationn.id != operation.id){
                     isValid = false;
                 }
             });
